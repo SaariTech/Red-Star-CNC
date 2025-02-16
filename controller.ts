ece3e04f1163d66e6809339ca0e2881a48396d1b
Jonatan Saari <5833573+SaariTech@users.noreply.github.com>
2025-02-16 07:57:04 +0100

    Update controller.ts

--- controller.ts
+++ controller.ts
@@ -9,8 +9,6 @@ console.log(c_yellow + ' ' + c_red_background + c_fill);
 const machine = require("./machine");
 const fs = require("fs");
 
-fs.writeFileSync("data/pause.txt", "0", { flag: "w" });
-
 setTimeout(() => {
 	const files = fs.readdirSync('./gcode');
 	files.forEach((file: string) => {
@@ -25,5 +23,5 @@ setTimeout(() => {
 			console.log('Maskin: Ignorerar ' + file);
 		}
 	});	
-	machine.Start(null, 0);
+	//machine.Start(null, 0);
 }, 100);
